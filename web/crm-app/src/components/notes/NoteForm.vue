9ca854fa7e61eb7062f841cd97679dc234b38226
Kris French <frenchie43972@gmail.com>
2026-03-23 16:16:45 +0900

    Scaffolded the frontend

---
+++ web/crm-app/src/components/notes/NoteForm.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <h1>Note Form</h1>
+</template>
+
+<style scoped></style>
---
+++ web/crm-app/src/components/notes/NoteList.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <h1>Note List</h1>
+</template>
+
+<style scoped></style>
