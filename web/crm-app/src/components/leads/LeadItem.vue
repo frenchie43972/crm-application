bb47cefd1762e470f488691ff4a4c784d3f64a66
Kris French <frenchie43972@gmail.com>
2026-03-26 08:38:36 +0900

    no real update

--- web/crm-app/src/components/leads/LeadItem.vue
+++ web/crm-app/src/components/leads/LeadItem.vue
@@ -1,6 +1,22 @@
 <script setup>
 import { useLeadsStore } from '@/stores/leads.store'
 import { useRouter } from 'vue-router'
+
+/**
+ * defineProps is part of Vue's <script setup> syntax.
+ *
+ * It declares the props the component expects.
+ * In this case we require a task object.
+ */
+const { lead } = defineProps({
+  lead: {
+    type: Object,
+    required: true,
+  },
+})
+
+const router = useRouter()
+const leadStore = useLeadsStore()
 </script>
 
 <template>
